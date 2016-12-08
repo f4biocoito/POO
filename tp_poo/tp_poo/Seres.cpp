379995936047a1bb295e12e269647f325c7c644a
Rafael Falcão <Rafael Falco@DESKTOP-4NMRRVI>
2016-12-08 19:44:59 +0000

    adicionado os seres

--- tp_poo/tp_poo/Seres.cpp
+++ tp_poo/tp_poo/Seres.cpp
@@ -1,2 +1,67 @@
 
 #include "Seres.h"
+
+void Seres::move()//faltam verifica??es
+{
+	for (int i = 0; i<velocidade;i++)
+	{
+		if (cordX != cordDestinoX&&cordY != cordDestinoY)
+		{
+			if (cordX>cordDestinoX&&cordY>cordDestinoY)
+			{
+				cordX--;
+				cordY--;
+			}
+			else if (cordX>cordDestinoX&&cordY<cordDestinoY)
+			{
+				cordX--;
+				cordY++;
+			}
+			else if (cordX<cordDestinoX&&cordY>cordDestinoY)
+			{
+				cordX++;
+				cordY--;
+			}
+			else if (cordX<cordDestinoX&&cordY<cordDestinoY)
+			{
+				cordX++;
+				cordY++;
+			}
+		}
+		else if (cordX != cordDestinoX&&cordY == cordDestinoY)
+		{
+			if (cordX>cordDestinoX)
+			{
+				cordX--;
+			}
+			else if (cordX<cordDestinoX)
+			{
+				cordX++;
+			}
+		}
+		else if (cordY != cordDestinoY&&cordX == cordDestinoX)
+		{
+			if (cordY>cordDestinoY)
+			{
+				cordY--;
+			}
+			else if (cordY<cordDestinoY)
+			{
+				cordY++;
+			}
+		}
+		else
+		{
+		}
+	}
+}
+
+void Unidade::setAtacar(int xAlvo, int yAlvo, string nome)
+{
+	alvo = nome;
+	cordAlvoX = xAlvo;
+	cordAlvoY = yAlvo;
+	cordDestinoX = xAlvo;
+	cordDestinoY = yAlvo;
+}
+
--- tp_poo/tp_poo/Seres.h
+++ tp_poo/tp_poo/Seres.h
@@ -3,9 +3,60 @@
 
 #include <string>
 #include <iostream>
-
+#include "Consola.h"
 using namespace std;
 
+class Seres
+{
+protected:
+	string id;
+	int hp;
+	int dano;
+	int defesa;
+	int velocidade;
+	int capCarga;
+	int cordX;
+	int cordY;
+	int cordDestinoX;
+	int cordDestinoY;
+	int cordAlvoX;
+	int cordAlvoY;
+	string alvo;
+
+	bool wandering;
+	string status; //estado da unidade - atacar, fugir, defender
+
+
+public:
+	// Unidade(int x, int y, string ident) { id = ident;cordDestinoX = x; cordDestinoY = y; cordX = x;cordY = y;cordAlvoX = -1;cordAlvoY = -1;alvo = ""; } 
+
+	//Funcoes get
+
+	string getId() { return id; }
+	int getHP() { return hp; }
+	int getDano() { return dano; }
+	int getDefesa() { return defesa; }
+	int getVelocidade() { return velocidade; };
+	int getCordX() { return cordX; }
+	int getCordY() { return cordY; }
+	int getCordDestinoX() { return cordDestinoX; }
+	int getCordDestinoY() { return cordDestinoY; }
+	string getAlvo() { return alvo; }
+	int getCordAlvoX() { return cordAlvoX; }
+	int getCordAlvoY() { return cordAlvoY; }
+	bool getWandering() { return wandering; }
+	string getStatus() { return status; }
+	int getCargaMax() { return capCarga; }
+
+	//Set
+	void setHP(int h) { hp = h; }
+	void setWandering(bool deambula) { wandering = deambula; }
+	void setStatus(string estado) { status = estado; }
+	void setCordDestinoX(int x) { cordDestinoX = x; }
+	void setCordDestinoY(int y) { cordDestinoY = y; }
+
+	void move();
+	
 
 
 
